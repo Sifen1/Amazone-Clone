2383e2365adb587b983e6412665be3bfc1ef9131
sifen <sifaman4@gmail.com>
2025-03-13 11:03:02 +0300

    First commit - backend

--- functions/index.js
+++ functions/index.js
@@ -17,44 +17,92 @@
 // //   logger.info("Hello logs!", {structuredData: true});
 // //   response.send("Hello from Firebase!");
 // // });
-const {onRequest} = require("firebase-functions/v2/https");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+// const express = require("express")
+// const cors =require("cors")
+// const dotenv =require("dotenv")
+// dotenv.config();
+// const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+
+// const app = express()
+// app.use(cors({origin:true}))
+// app.use(express.json())
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({
+//       message: "Success !",
+//     });
+//   });
+
+//   app.post("/payment/create", async (req, res) => {
+//     const total = parseInt(req.query.total);
+//     if (total > 0) {
+//       const paymentIntent = await stripe.paymentIntents.create({
+//         amount:total,
+//         currency:"usd",
+//       });
+//       //console.log(paymentIntent);
+
+
+//       res.status(201).json({
+//         clientsecret:paymentIntent.client_secret,
+//       });
+//     }else{
+//       res.status(403).json({
+//         message:"total must be greater than 0",
+//       });
+//     }
+// });
+
+
+// exports.api = onRequest(app);
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const express = require("express")
-const cors =require("cors")
-const dotenv =require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_KEY)
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-const app = express()
-app.use(cors({origin:true}))
-app.use(express.json())
-
+// Simple health check route
 app.get("/", (req, res) => {
-    res.status(200).json({
-      message: "Success !",
-    });
+  res.status(200).json({
+    message: "Success!",
   });
-
-  app.post("/payment/create", async (req, res) => {
-    const total = parseInt(req.query.total);
-    if (total > 0) {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount:total,
-        currency:"usd",
-      });
-      //console.log(paymentIntent);
-
-
-      res.status(201).json({
-        clientsecret:paymentIntent.client_secret,
-      });
-    }else{
-      res.status(403).json({
-        message:"total must be greater than 0",
-      });
-    }
 });
 
+// Payment route
+app.post("/payment/create", async (req, res) => {
+  // Now, access the `total` from the request body
+  const { total } = req.body;  // Use req.body instead of req.query
+
+  if (total > 0) {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+
+      res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      console.error("Error creating payment intent:", error);
+      res.status(500).json({
+        message: "Failed to create payment intent",
+      });
+    }
+  } else {
+    res.status(403).json({
+      message: "Total must be greater than 0",
+    });
+  }
+});
 
 exports.api = onRequest(app);
